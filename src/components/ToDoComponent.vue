c7d508de632cd8f0f0a9f0f8de3adc390ead2775
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-08 11:26:53 -0300

    table icons + binding ok

--- src/components/ToDoComponent.vue
+++ src/components/ToDoComponent.vue
@@ -6,7 +6,21 @@
         <!-- <h1>Hola desde ToDo Component</h1> -->
         <!-- <h3>{{ $store.state.myName }}</h3>
         <h3>{{ $store.getters.getFullName }}</h3>
-        <h3>{{ $store.state.responseApiBackend[0].name }}</h3> -->
+        <h3>{{ $store.state.responseApiBackend[0].name }}</h3>-->
+
+        <v-form ref="form" lazy-validation>
+          <v-text-field
+            @keydown.enter="addToDoItem(toDoItemName)"
+            v-model="toDoItemName"
+            label="To Do Item"
+            required
+          ></v-text-field>
+
+          <v-btn color="primary" class="mr-6" v-on:click="addToDoItem(toDoItemName)">
+            Add
+            <v-icon dark right>mdi-plus</v-icon>
+          </v-btn>
+        </v-form>
       </v-col>
     </v-row>
 
@@ -16,16 +30,60 @@
           <template v-slot:default>
             <thead>
               <tr>
-                <th class="text-left">Id</th>
-                <th class="text-left">Name</th>
-                <th class="text-left">isCompleted</th>
+                <th>
+                  <strong>ID</strong>
+                </th>
+                <th>
+                  <strong>NAME</strong>
+                </th>
+                <th>
+                  <strong>STATE</strong>
+                </th>
+                <th>
+                  <strong>DELETE</strong>
+                </th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="toDoitem in $store.state.responseApiBackend" :key="toDoitem.id">
-                <td>{{ toDoitem.id }}</td>
-                <td>{{ toDoitem.name }}</td>
-                <td>{{ toDoitem.isCompleted }}</td>
+              <tr v-for="toDoItem in $store.state.responseApiBackend" :key="toDoItem.id">
+                <td>{{ toDoItem.id }}</td>
+                <td>{{ toDoItem.name }}</td>
+
+                <td>
+                  <v-btn
+                    v-if="toDoItem.isCompleted"
+                    v-on:click="markToDoItemAsIncompleted(toDoItem.id)"
+                    class="ma-2"
+                    color="green"
+                    dark
+                  >
+                    Done
+                    <v-icon dark right>mdi-checkbox-marked-circle</v-icon>
+                  </v-btn>
+
+                  <v-btn
+                    v-else
+                    v-on:click="markToDoItemAsDone(toDoItem.id)"
+                    class="ma-2"
+                    color="warning"
+                    dark
+                  >
+                    Incompleted
+                    <v-icon dark right>mdi-minus-circle</v-icon>
+                  </v-btn>
+                </td>
+                <td>
+                  <v-btn
+                    v-on:click="deleteToDoItemById(toDoItem.id)"
+                    class="mx-2"
+                    fab
+                    dark
+                    small
+                    color="red"
+                  >
+                    <v-icon dark>mdi-delete</v-icon>
+                  </v-btn>
+                </td>
               </tr>
             </tbody>
           </template>
@@ -37,6 +95,36 @@
 
 <script>
 export default {
+  data: () => ({
+    toDoItemName: ''
+
+  }),
+  methods: {
+    addToDoItem(toDoItemName) {
+
+      alert(toDoItemName)
+      // TodoItemService.addToDoItem(this.toDoItemName);
+      // refreshTable();
+    },
+    deleteToDoItemById(todoItemId) {
+
+      alert(todoItemId)
+      // TodoItemService.deleteToDoItemById(todoItemId);
+      // refreshTable();
+    },
+    markToDoItemAsIncompleted(toDoItemId) {
+
+      alert(toDoItemId)
+      // TodoItemService.markToDoItemAsIncompleted(toDoItemId);
+      // refreshTable();
+    },
+    markToDoItemAsDone(toDoItemId) {
+
+      alert(toDoItemId)
+      // TodoItemService.markToDoItemAsDone(toDoItemId);
+      // refreshTable();
+    }
+  },
 
 }
 </script>
