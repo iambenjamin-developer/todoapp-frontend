09e9eb3599b0e45783f3824aceaa3a826324c34b
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-08 12:38:23 -0300

    delete internal memory data

--- src/store/toDoModule/state.js
+++ src/store/toDoModule/state.js
@@ -3,17 +3,6 @@ export default () => ({
     myName: 'Benjamin',
     myLastName: 'Correa',
     isLoading: true,
-    toDoList: [
-        {
-            id: 2001,
-            name: "tarea 2001 memoria interna",
-            isCompleted: true
-        },
-        {
-            id: 2007,
-            name: "tarea 2007 memoria interna",
-            isCompleted: false
-        },
-    ]
+    toDoList: []
 
 })
